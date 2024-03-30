132628c329f56c616d76c7d496a614640be33222
laoloser <slaux86@gmail.com>
2024-03-30 01:01:41 -0700

    first commit

--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -11,15 +11,15 @@ class Vehicle {
         this.numberOfWheels = 0;
     }
 
-    start() {
-        if (this.fuel > 0) {
-            return this.started = true;
-            console.log("engine started...!!!");
-        } else {
-            return this.started = false;
-            console.log("engine cannot start...");
-        }
-    }
+    // start() {
+    //     if (this.fuel > 0) {
+    //         return this.started = true;
+    //         console.log("engine started...!!!");
+    //     } else {
+    //         return this.started = false;
+    //         console.log("engine cannot start...");
+    //     }
+    // }
     accelerate() {
         if (this.started) {
             if (this.fuel > 0) {
@@ -86,6 +86,6 @@ class Vehicle {
 }
 
 //This exports things you want to use from this "module", more info in readme
-module.exports = {
-    Vehicle
-}
+// module.exports = {
+//     Vehicle
+// }
